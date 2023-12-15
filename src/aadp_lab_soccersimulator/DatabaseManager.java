af1f0a0c48ee8c5ef42e9349dd8bf26e4433a2cb
GuilhermeFCdeLima <153609203+GuilhermeFCdeLima@users.noreply.github.com>
2023-12-15 14:08:59 +0000

    Add the result set getTeamPlayers

--- src/aadp_lab_soccersimulator/DatabaseManager.java
+++ src/aadp_lab_soccersimulator/DatabaseManager.java
@@ -2,10 +2,12 @@ package aadp_lab_soccersimulator;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseManager {
+
     private String dbName = "world_cup";
     private String DB_URL = "jdbc:mysql://localhost/" + dbName;
     private String USER = "root";
@@ -32,12 +34,12 @@ public class DatabaseManager {
             for (String team : teams) {
                 stmt.execute(
                         "CREATE TABLE IF NOT EXISTS " + team + " ("
-                                + "name VARCHAR(30) NOT NULL,"
-                                + "number INT NOT NULL PRIMARY KEY,"
-                                + "birth VARCHAR(30),"
-                                + "position VARCHAR(30),"
-                                + "goalsScored INT,"
-                                + "background TEXT(1000));"
+                        + "name VARCHAR(30) NOT NULL,"
+                        + "number INT NOT NULL PRIMARY KEY,"
+                        + "birth VARCHAR(30),"
+                        + "position VARCHAR(30),"
+                        + "goalsScored INT,"
+                        + "background TEXT(1000));"
                 );
             }
         } catch (SQLException e) {
@@ -46,20 +48,34 @@ public class DatabaseManager {
     }
 
     void addPlayer(String teamName, String name, int number, String birth, String position, int goalsScored, String background) {
-    Connection conn = getConnection();
+        Connection conn = getConnection();
 
-    try {
-        Statement stmt = conn.createStatement();
-        stmt.execute(
-                String.format("INSERT INTO %s (name, number, birth, position, goalsScored, background) "
-                                + "VALUES (\"%s\", %d, \"%s\", \"%s\", %d,  \"%s\") ;",
-                        teamName, name, number, birth, position, goalsScored, background)
-        );
-    } catch (SQLException e) {
-        e.printStackTrace();
-        throw new RuntimeException(e);
-    
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute(
+                    String.format("INSERT INTO %s (name, number, birth, position, goalsScored, background) "
+                            + "VALUES (\"%s\", %d, \"%s\", \"%s\", %d,  \"%s\") ;",
+                            teamName, name, number, birth, position, goalsScored, background)
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+
         }
-    
+
+    }
+
+    ResultSet getTeamPlayers(String teamName) {
+        Connection conn = getConnection();
+
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * from " + teamName + ";");
+            return rs;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
